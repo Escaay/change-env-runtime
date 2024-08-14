ef5be875db771b4ba2ede3eb9f573c937146b34e
qiuwenjing03 <qiuwenjing03@meituan.com>
2024-08-14 11:09:12 +0800

    完善readme

--- change-env-runtime.config.js
+++ change-env-runtime.config.js
@@ -1,24 +1,24 @@
 // UDM
 module.exports = {
   envPath: "./.env",
-  // tmpList: [
-  //     {
-  //         name: '泳道',
-  //         value: 'VUE_APP_SWIMLANE'
-  //     },
-  //     {
-  //         name: '环境',
-  //         value: 'VUE_APP_UECP_ENV'
-  //     },
-  //     {
-  //         name: 'URM_URL',
-  //         value: 'VUE_APP_UDM_URL'
-  //     },
-  //     {
-  //         name: 'MQTT_SERVER',
-  //         value: 'VUE_APP_MQTT_SERVER'
-  //     }
-  // ]
+  tmpList: [
+    {
+      name: "泳道",
+      value: "VUE_APP_SWIMLANE",
+    },
+    {
+      name: "环境",
+      value: "VUE_APP_UECP_ENV",
+    },
+    {
+      name: "URM_URL",
+      value: "VUE_APP_UDM_URL",
+    },
+    {
+      name: "MQTT_SERVER",
+      value: "VUE_APP_MQTT_SERVER",
+    },
+  ],
 };
 
 // UMS
--- readme.md
+++ readme.md
@@ -1,7 +1,8 @@
 在项目根目录下创建文件change-env-runtime.config.js
 模板如下
 
-module.exports = {
+
+`module.exports = {
 envPath: './.env',
 tmpList: [
 {
@@ -21,13 +22,15 @@ name: 'MQTT_SERVER',
 value: 'VUE_APP_MQTT_SERVER'
 }
 ]
-}
+}`
 
 运行npx cvrt start
 
 更新日志
-2024.8.13 1.通过降低依赖版本兼容低版本库
-2.esm和cjs都转换为cjs，达到二者混用的效果3.优化正则，匹配多种格式4.增加配置文件change-env.config.js,自由配置解析字段和env文件路径
+2024.8.13
+1.通过降低依赖版本兼容低版本库
+2.esm和cjs都转换为cjs，达到二者混用的效果
+3.优化正则，匹配多种格式4.增加配置文件change-env.config.js,自由配置解析字段和env文件路径
 
 2024.8.14
 添加默认模板列表（默认解析全部）
